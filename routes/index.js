9514bd60a72abeda85e76160c05ccea29cd2693e
Matt Burch <matthew.burch@adroll.com>
2020-07-21 15:59:25 -0600

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+const webpush = require('web-push')
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* SUBSCRIBE to push notifications */
+router.post('/notifications/subscribe', (req, res) => {
+  const subscription = req.body
+
+  console.log("Received subscription",subscription);
+
+  const payload = JSON.stringify({
+    title: 'Hello from Express',
+    body: 'Send from an Express application',
+  })
+
+  webpush.sendNotification(subscription, payload)
+    .then(result => console.log(result))
+    .catch(e => console.log("ERROR!", e.stack))
+
+  res.status(200).json({'success': true})
+});
+
+module.exports = router;
